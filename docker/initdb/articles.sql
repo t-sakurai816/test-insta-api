a052e9a8fbc81098e01f5fced0d097309282a8b1
t-sakurai816 <poppun_0816@icloud.com>
2021-03-07 21:52:22 +0900

    DBを作成して、テストデータを入れる

--- docker/initdb/articles.sql
+++ docker/initdb/articles.sql
@@ -1,12 +1,106 @@
 SET CHARACTER_SET_CLIENT = utf8mb4;
 SET CHARACTER_SET_CONNECTION = utf8mb4;
 
-CREATE TABLE test (
-  id int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  body varchar(255) NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `sampledb`. `test_t.saku` ( `id` BIGINT NOT NULL AUTO_INCREMENT , `date` DATE NOT NULL , `like_num` BIGINT NOT NULL , `comment_num` BIGINT NOT NULL , `post_num` INT NOT NULL , `follower_num` INT NOT NULL , `impression_num` BIGINT NOT NULL , `reach_num` BIGINT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 
-INSERT INTO test (id, body) VALUES
-(1, 'テスト1'),
-(2, 'テスト2');
+INSERT INTO `test_t.saku`(
+    `id`,
+    `date`,
+    `like_num`,
+    `comment_num`,
+    `post_num`,
+    `follower_num`,
+    `impression_num`,
+    `reach_num`
+)
+VALUES
+(
+    NULL,
+    DATE_SUB(CURRENT_DATE, INTERVAL 1 DAY),
+    FLOOR(20 + RAND() * 40),
+    FLOOR(5 + RAND() * 20),
+    FLOOR(5 + RAND() * 15),
+    FLOOR(20 + RAND() * 50),
+    FLOOR(30 + RAND() * 50),
+    FLOOR(30 + RAND() * 50)
+),
+(
+    NULL,
+    DATE_SUB(CURRENT_DATE, INTERVAL 2 DAY),
+    FLOOR(20 + RAND() * 40),
+    FLOOR(5 + RAND() * 20),
+    FLOOR(5 + RAND() * 15),
+    FLOOR(20 + RAND() * 50),
+    FLOOR(30 + RAND() * 50),
+    FLOOR(30 + RAND() * 50)
+),
+(
+    NULL,
+    DATE_SUB(CURRENT_DATE, INTERVAL 3 DAY),
+    FLOOR(20 + RAND() * 40),
+    FLOOR(5 + RAND() * 20),
+    FLOOR(5 + RAND() * 15),
+    FLOOR(20 + RAND() * 50),
+    FLOOR(30 + RAND() * 50),
+    FLOOR(30 + RAND() * 50)
+),
+(
+    NULL,
+    DATE_SUB(CURRENT_DATE, INTERVAL 4 DAY),
+    FLOOR(20 + RAND() * 40),
+    FLOOR(5 + RAND() * 20),
+    FLOOR(5 + RAND() * 15),
+    FLOOR(20 + RAND() * 50),
+    FLOOR(30 + RAND() * 50),
+    FLOOR(30 + RAND() * 50)
+),
+(
+    NULL,
+    DATE_SUB(CURRENT_DATE, INTERVAL 5 DAY),
+    FLOOR(20 + RAND() * 40),
+    FLOOR(5 + RAND() * 20),
+    FLOOR(5 + RAND() * 15),
+    FLOOR(20 + RAND() * 50),
+    FLOOR(30 + RAND() * 50),
+    FLOOR(30 + RAND() * 50)
+),
+(
+    NULL,
+    DATE_SUB(CURRENT_DATE, INTERVAL 6 DAY),
+    FLOOR(20 + RAND() * 40),
+    FLOOR(5 + RAND() * 20),
+    FLOOR(5 + RAND() * 15),
+    FLOOR(20 + RAND() * 50),
+    FLOOR(30 + RAND() * 50),
+    FLOOR(30 + RAND() * 50)
+),
+(
+    NULL,
+    DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY),
+    FLOOR(20 + RAND() * 40),
+    FLOOR(5 + RAND() * 20),
+    FLOOR(5 + RAND() * 15),
+    FLOOR(20 + RAND() * 50),
+    FLOOR(30 + RAND() * 50),
+    FLOOR(30 + RAND() * 50)
+),
+(
+    NULL,
+    DATE_SUB(CURRENT_DATE, INTERVAL 8 DAY),
+    FLOOR(20 + RAND() * 40),
+    FLOOR(5 + RAND() * 20),
+    FLOOR(5 + RAND() * 15),
+    FLOOR(20 + RAND() * 50),
+    FLOOR(30 + RAND() * 50),
+    FLOOR(30 + RAND() * 50)
+),
+(
+    NULL,
+    DATE_SUB(CURRENT_DATE, INTERVAL 9 DAY),
+    FLOOR(20 + RAND() * 40),
+    FLOOR(5 + RAND() * 20),
+    FLOOR(5 + RAND() * 15),
+    FLOOR(20 + RAND() * 50),
+    FLOOR(30 + RAND() * 50),
+    FLOOR(30 + RAND() * 50)
+);
