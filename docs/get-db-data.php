d86a6b1682cab08844aee5e816490379a5642594
t-sakurai816 <poppun_0816@icloud.com>
2021-03-12 19:41:53 +0900

    関数化

--- docs/get-db-data.php
+++ docs/get-db-data.php
@@ -1,34 +1,34 @@
 <?php
 echo "DBからデータ取得して各値を表示する" . "<br>";
 
-try {
-  /// DB接続を試みる
-  $dsn = 'mysql:dbname=sampledb;host=myapp-db';
-  $dbh = new PDO($dsn, $_ENV['MYSQL_USER'], $_ENV['MYSQL_PASSWORD']);
-  // echo "DB接続成功";
 
-	// SQL作成
-	$sql = "SELECT `like_num` FROM `test_t.saku` WHERE `date` >= (NOW() - INTERVAL 7 DAY)";
-  // echo $sql.'<br>';
-
-  // SQL実行
-  $stmt = $dbh->prepare($sql);
-  $stmt->execute();
+function selectDB($table){
+  try {
+    /// DB接続を試みる
+    $dsn = 'mysql:dbname=sampledb;host=myapp-db';
+    $dbh = new PDO($dsn, $_ENV['MYSQL_USER'], $_ENV['MYSQL_PASSWORD']);
+    // echo "DB接続成功";
   
-  // 結果の取得
-  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  // var_dump($result).'<br>';;
+    // SQL作成
+    $sql = "SELECT $table FROM `test_t.saku` WHERE `date` >= (NOW() - INTERVAL 8 DAY)";
+    echo "SQL : " . $sql . "<br>";
+  
+    // SQL実行
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+    
+    // 結果の取得
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // var_dump($result).'<br>';;
+    return $result;
+  
+  } catch(PDOException $e) {
+    echo $e->getMessage();
+    die();
+  }
+};
 
-} catch(PDOException $e) {
-	echo $e->getMessage();
-	die();
-}
-
-echo "SQL : " . $sql . "<br>";
-// echo('<pre>');
-//   var_dump($result) . "<br>";
-// echo('</pre>');
-
+$result = selectDB('like_num');
 for($i = 0; $i<7; $i++){
   $like_num[$i] = $result[$i]['like_num'];
   echo "いいね数 : " . $like_num[$i] . "<br>";
